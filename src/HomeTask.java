0e88ef3c9f458079c61ce1823b4e36b54f967650
ihor <ihprishchepa@gmail.com>
2023-09-08 22:00:11 +0300

    add 2 methods+loops

--- src/HomeTask.java
+++ src/HomeTask.java
@@ -15,7 +15,7 @@ class MyException extends Exception {
 
 public class HomeTask {
     static double divineDouble(double a, double b) throws ArithmeticException {
-        if (a == 0 | b == 0) throw new ArithmeticException();
+        if (a == 0 || b == 0) throw new ArithmeticException();
         else return a / b;
     }
 
@@ -25,27 +25,32 @@ public class HomeTask {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        try {
-            System.out.println("Введите число в диапазоне [1;100]");
-            int value1 = getNumber(sc.nextInt());
-            System.out.println(value1);
-        } catch (MyException e) {
-            System.out.println("Исключение перехвачено - " + e.toString());
-        } catch (InputMismatchException e) {
-            System.out.println("Вы ввели строку " + e);
-        }
-
-        try {
-            System.out.println("Введите 2 числа: ");
-            double value2 = sc.nextDouble();
-            double value3 = sc.nextDouble();
-            System.out.println(divineDouble(value2, value3));
-        } catch (InputMismatchException e) {
-            System.out.println("Вы ввели строку " + e);
-        } catch (ArithmeticException e) {
-            System.out.println("Деление на 0 " + e);
-        }
+        double a = 0;
+        double b = 0;
+        do {
+            try {
+                System.out.println("Введите 2 числа:");
+                a = new Scanner(System.in).nextDouble();
+                b = new Scanner(System.in).nextDouble();
+                System.out.println(divineDouble(a, b));
+            } catch (InputMismatchException e) {
+                System.out.println("Вы ввели строку: " + e);
+            } catch (ArithmeticException e) {
+                System.out.println("Деление на 0 " + e);
+            }
+        } while ((a != 0) && (b != 0));
+        int value1 = 0;
+        do {
+            try {
+                System.out.println("Введите целое число в диапазоне от 1 до 100:");
+                value1 = new Scanner(System.in).nextInt();
+                System.out.println(getNumber(value1));
+            } catch (MyException e) {
+                System.out.println("Исключение перехвачено: " + e);
+            } catch (InputMismatchException e) {
+                System.out.println("Вы ввели строку: " + e);
+            }
+        } while (value1 != 0);
     }
 }
 
