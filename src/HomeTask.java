d110ec3cb0ece4fc78500e98b82ee0b3e0fe0615
ihor <ihprishchepa@gmail.com>
2023-09-09 15:58:32 +0300

    end hometask

--- src/HomeTask.java
+++ src/HomeTask.java
@@ -25,33 +25,24 @@ public class HomeTask {
     }
 
     public static void main(String[] args) {
-        double a = 0;
-        double b = 0;
-        do {
-            try {
-                System.out.println("Введите 2 числа:");
-                a = new Scanner(System.in).nextDouble();
-                b = new Scanner(System.in).nextDouble();
-                System.out.println(divineDouble(a, b));
-            } catch (InputMismatchException e) {
-                System.out.println("Вы ввели строку: " + e);
-            } catch (ArithmeticException e) {
-                System.out.println("Деление на 0 " + e);
-            }
-        } while ((a != 0) && (b != 0));
-        int value1 = 0;
-        do {
-            try {
-                System.out.println("Введите целое число в диапазоне от 1 до 100:");
-                value1 = new Scanner(System.in).nextInt();
-                System.out.println(getNumber(value1));
-            } catch (MyException e) {
-                System.out.println("Исключение перехвачено: " + e);
-            } catch (InputMismatchException e) {
-                System.out.println("Вы ввели строку: " + e);
-            }
-        } while (value1 != 0);
+        try {
+            System.out.println("Введите 2 числа:");
+            double a = new Scanner(System.in).nextDouble();
+            double b = new Scanner(System.in).nextDouble();
+            System.out.println(divineDouble(a, b));
+        } catch (InputMismatchException e) {
+            System.out.println("Вы ввели строку: " + e);
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на 0 " + e);
+        }
+        try {
+            System.out.println("Введите целое число в диапазоне от 1 до 100:");
+            int value1 = new Scanner(System.in).nextInt();
+            System.out.println(getNumber(value1));
+        } catch (MyException e) {
+            System.out.println("Исключение перехвачено: " + e);
+        } catch (InputMismatchException e) {
+            System.out.println("Вы ввели строку: " + e);
+        }
     }
 }
-
-
